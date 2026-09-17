01ac9f433d2feb0b3b419b78ef6dcd5c9495f395
Junaid Oluwatobi <tobijunaid2@gmail.com>
2026-09-17 16:13:08 +0100

    Built System Information CLI tool

---
+++ week-01/system-info/main.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+from datetime import datetime
+import platform
+import sys
+import psutil
+
+def main():
+    
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    memory = psutil.virtual_memory()
+    disk = psutil.disk_usage("C:\\")
+    
+    print("=" * 50)
+    print("           SYSTEM INFORMATION")
+    print("=" * 50)
+
+    print(f"Operating System: {platform.system()}")
+    print(f"OS Version: {platform.version()}")
+    print(f"Machine: {platform.machine()}")
+    print(f"Processor: {platform.processor()}")
+    print(f"Python Version: {sys.version}")
+    print(f"Current Working Directory: {Path.cwd()}")
+    print("-" * 50)
+    
+    print(f"CPU Cores: {psutil.cpu_count(logical=True)}")
+    print(f"CPU Frequency (MHz): {psutil.cpu_freq().current}")
+    print(f"Total RAM: {round(memory.total / (1024 ** 3), 2)} GB")
+    print(f"Available RAM: {round(memory.available / (1024 ** 3), 2)} GB")
+    print(f"Used RAM: {round(memory.used / (1024 ** 3), 2)} GB")
+    print(f"CPU Usage (%): {psutil.cpu_percent(interval=1)}%")
+    print(f"Disk Usage (%): {disk.percent}")
+    print(f"Disk Total: {round((disk.total) / (1024 ** 3), 2)} GB")
+
+    print(f"Generated at: {current_time}")
+    
+    print("-" * 50)
+
+if __name__ == "__main__":    
+    main()
