49b5871c8497d926d97dc90d0cf2d9baeaf103fc
Junaid Oluwatobi <tobijunaid2@gmail.com>
2026-09-23 15:54:30 +0100

    wrote program to read students from a file and save results to another file. The program reads student names and scores from "students.txt", prints the scores, and saves the results of students who scored 80 or above to "results.txt".

--- week-02/file-practice/main.py
+++ week-02/file-practice/main.py
@@ -1,24 +1,34 @@
 from pathlib import Path
 
-# Gets the directory where main.py lives
 current_dir = Path(__file__).parent
 file_path = current_dir / "students.txt"
+result_path = current_dir / "results.txt"
 
+def save_results(name, score):
+    if score >= 80:
+        print(f"{name} scored more than 80: {score}")
+        with open(result_path, "a") as file :
+                file.write(f"{name},{score}\n")
 
-with open(file_path, "r") as file:
-    # content = file.read()
-    lines = file.readlines()
-    
-    for line in lines:
-        # print(line.strip().split(","))
-        name, score = line.strip().split(",")
-        score = int(score)
-        print(f"{name} scored: {score}")
-        
-        if score >= 80:
-            print(f"{name} scored more than 80: {score}")
-        
         # Alternative way to do it without unpacking
         # name = line.strip().split(",")[0]
         # score = int(line.strip().split(",")[1])
         # print(f"{name} scored: {score}")
+        
+def read_students():
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+    
+    for line in lines:
+        name, score = line.strip().split(",")
+        score = int(score)
+        print(f"{name} scored: {score}")
+        save_results(name, score)
+
+def main():
+    with open(result_path, "w") as file:
+        pass  # Clear the results file before writing new results
+    read_students()
+    
+if __name__ == "__main__":
+    main()
